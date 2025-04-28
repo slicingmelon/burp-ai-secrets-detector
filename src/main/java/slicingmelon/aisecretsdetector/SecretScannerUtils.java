a9aa09312da7cd67d07677fd5848ee83d469c927
slicingmelon <petre@tutanota.com>
2025-04-28 15:53:37 +0300

    updates

--- src/main/java/slicingmelon/aisecretsdetector/SecretScannerUtils.java
+++ src/main/java/slicingmelon/aisecretsdetector/SecretScannerUtils.java
@@ -108,6 +108,10 @@ public class SecretScannerUtils {
         
         addPattern("AWS Access Key", 
             "\\b((?:A3T[A-Z0-9]|AKIA|ASIA|ABIA|ACCA)[A-Z0-9]{16,20})\\b");
+
+        // Bcrypt Hash pattern
+        addPattern("Bcrypt Hash", 
+            "\\b\\$2[abxy]\\$\\d{2}\\$[./A-Za-z0-9]{53}\\b");
         
         addPattern("Fastly API Key", 
             "(?i)[\\w.-]{0,50}?(?:fastly)(?:[ \\t\\w.-]{0,20})[\\s'\"]{0,3}(?:=|>|:{1,3}=|\\|\\||:|=>|\\?=|,)[\\x60'\"\\s=]{0,5}([a-z0-9=_\\-]{32})(?:[\\x60'\"\\s;]|\\\\[nr]|$)");
@@ -138,13 +142,17 @@ public class SecretScannerUtils {
             "(?i)[\\w.-]{0,50}?(?:mailgun)(?:[ \\t\\w.-]{0,20})[\\s'\"]{0,3}(?:=|>|:{1,3}=|\\|\\||:|=>|\\?=|,)[\\x60'\"\\s=]{0,5}(key-[a-f0-9]{32})(?:[\\x60'\"\\s;]|\\\\[nr]|$)");
         
         addPattern("OpenAI API Key", 
-            "\\b(sk-[a-zA-Z0-9]{20}T3BlbkFJ[a-zA-Z0-9]{20})(?:[\\x60'\"\\s;]|\\\\[nr]|$)");
+            "\\b(sk-(?:proj|svcacct|admin)-(?:[A-Za-z0-9_-]{74}|[A-Za-z0-9_-]{58})T3BlbkFJ(?:[A-Za-z0-9_-]{74}|[A-Za-z0-9_-]{58})\\b|sk-[a-zA-Z0-9]{20}T3BlbkFJ[a-zA-Z0-9]{20})(?:[\\x60'\"\\s;]|\\\\[nr]|$)");
         
         addPattern("NPM Token (modern)", 
             "npm_[\\dA-Za-z]{36}");
         
         addPattern("NPM Token (legacy)", 
             "//.+/:_authToken=[\\dA-Za-z_-]+");
+
+        // Postman API Token
+        addPattern("Postman API Token", 
+            "\\b(PMAK-(?i)[a-f0-9]{24}\\-[a-f0-9]{34})(?:[\\x60'\"\\s;]|\\\\[nr]|$)");
 
         // reCAPTCHA Site Key
         addPattern("reCAPTCHA Site Key", 
@@ -157,26 +165,62 @@ public class SecretScannerUtils {
         addPattern("Slack Token", 
             "xox[aboprs]-(?:\\d+-)+[\\da-z]+");
         
+        // Slack Tokens
+        addPattern("Slack App Token", 
+            "(?i)xapp-\\d-[A-Z0-9]+-\\d+-[a-z0-9]+");
+            
+        addPattern("Slack Bot Token", 
+            "xoxb-[0-9]{10,13}-[0-9]{10,13}[a-zA-Z0-9-]*");
+            
+        addPattern("Slack Legacy Bot Token", 
+            "xoxb-[0-9]{8,14}-[a-zA-Z0-9]{18,26}");
+            
+        addPattern("Slack User Token", 
+            "xox[pe](?:-[0-9]{10,13}){3}-[a-zA-Z0-9-]{28,34}");
+            
+        addPattern("Slack Legacy Token", 
+            "xox[os]-\\d+-\\d+-\\d+-[a-fA-F\\d]+");
+        
         addPattern("Slack Config Access Token", 
             "(?i)xoxe.xox[bp]-\\d-[A-Z0-9]{163,166}");
+        
+        addPattern("Slack Config Refresh Token", 
+            "(?i)xoxe-\\d-[A-Z0-9]{146}");
         
         addPattern("Slack Legacy Workspace Token", 
             "xox[ar]-(?:\\d-)?[0-9a-zA-Z]{8,48}");
         
         addPattern("Slack Webhook URL", 
-            "(?:https?://)?hooks.slack.com/(?:services|workflows)/[A-Za-z0-9+/]{43,46}");
+            "(?:https?://)?hooks.slack.com/(?:services|workflows|triggers)/[A-Za-z0-9+/]{43,56}");
+            
+        // Microsoft Teams Webhook
+        addPattern("Microsoft Teams Webhook", 
+            "https://[a-z0-9]+\\.webhook\\.office\\.com/webhookb2/[a-z0-9]{8}-([a-z0-9]{4}-){3}[a-z0-9]{12}@[a-z0-9]{8}-([a-z0-9]{4}-){3}[a-z0-9]{12}/IncomingWebhook/[a-z0-9]{32}/[a-z0-9]{8}-([a-z0-9]{4}-){3}[a-z0-9]{12}");
         
         addPattern("SendGrid API Key", 
             "SG\\.[\\dA-Za-z_-]{22}\\.[\\dA-Za-z_-]{43}");
         
         addPattern("Stripe API Key", 
-            "[rs]k_live_[\\dA-Za-z]{24,247}");
+            "\\b((?:sk|rk)_(?:test|live|prod)_[a-zA-Z0-9]{10,99})(?:[\\x60'\"\\s;]|\\\\[nr]|$)");
         
         addPattern("Square Access Token", 
             "\\b((?:EAAA|sq0atp-)[\\w-]{22,60})(?:[\\x60'\"\\s;]|\\\\[nr]|$)");
         
         addPattern("Squarespace Access Token", 
             "(?i)[\\w.-]{0,50}?(?:squarespace)(?:[ \\t\\w.-]{0,20})[\\s'\"]{0,3}(?:=|>|:{1,3}=|\\|\\||:|=>|\\?=|,)[\\x60'\"\\s=]{0,5}([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})(?:[\\x60'\"\\s;]|\\\\[nr]|$)");
+        
+        // Shopify Access Tokens
+        addPattern("Shopify Access Token", 
+            "shpat_[a-fA-F0-9]{32}");
+            
+        addPattern("Shopify Custom Access Token", 
+            "shpca_[a-fA-F0-9]{32}");
+            
+        addPattern("Shopify Private App Access Token", 
+            "shppa_[a-fA-F0-9]{32}");
+            
+        addPattern("Shopify Shared Secret", 
+            "shpss_[a-fA-F0-9]{32}");
         
         addPattern("Twilio API Key", 
             "(?:AC|SK)[\\da-z]{32}");
@@ -186,6 +230,9 @@ public class SecretScannerUtils {
         
         addPattern("Intra42 Secret", 
             "s-s4t2(?:af|ud)-[\\da-f]{64}");
+        
+        addPattern("Zendesk Secret Key", 
+            "(?i)[\\w.-]{0,50}?(?:zendesk)(?:[ \\t\\w.-]{0,20})[\\s'\"]{0,3}(?:=|>|:{1,3}=|\\|\\||:|=>|\\?=|,)[\\x60'\"\\s=]{0,5}([a-z0-9]{40})(?:[\\x60'\"\\s;]|\\\\[nr]|$)");
         
         addPattern("Generic Private Key", 
             "(?i)-----BEGIN[ A-Z0-9_-]{0,100}PRIVATE KEY(?: BLOCK)?-----[\\s\\S-]{64,}?KEY(?: BLOCK)?-----");
