54a86e414cbc059b2b5be16001538e50ff843a4f
slicingmelon <petre@tutanota.com>
2025-06-03 19:51:59 +0300

    xargs

--- test-server/static/secrets.js
+++ test-server/static/secrets.js
@@ -2,5 +2,5 @@
 const API_SETTINGS = {
     timeout: 30000,
     retries: 3,
-    github_token: "ghp_mDhCRIihKErfufve0Oc0m6UL60qA3sgokFmY"
+    github_token: "ghp_wgHjkAM5QJ0k6OI5E0jc3HhYdFznOmIaZymG"
 };
