18d8c51added32740e21164302516b239e0d47d6
slicingmelon <petre@tutanota.com>
2025-06-11 16:39:19 +0300

    asa

--- test-server/static/secrets.js
+++ test-server/static/secrets.js
@@ -2,5 +2,5 @@
 const API_SETTINGS = {
     timeout: 30000,
     retries: 3,
-    github_token: "ghp_wgHjkAM5QJ0k6OI5E0jc3HhYdFznOmIaZymG"
+    github_token: "ghp_jMoG0S7rn19tpj33NSRTZjs3JblYqgAJjQ6Q"
 };
